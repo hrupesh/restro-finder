f2354f3643073526112516c595695175230fa899
hrupesh <rcashok0@gmail.com>
2020-06-29 01:42:56 +0530

    Added Custom StatusBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,7 +7,7 @@ export default function SearchScreen() {
   return (
     <View style={styles.container}>
       <SearchBar />
-      <StatusBar backgroundColor="#448AFFCF" ></StatusBar>
+      <StatusBar backgroundColor="#448AFF" ></StatusBar>
     </View>
   );
 }
