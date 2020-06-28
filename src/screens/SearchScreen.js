ca216c16cfd0c56e32b13d853ce49c9bbffc3ec5
hrupesh <rcashok0@gmail.com>
2020-06-29 02:20:16 +0530

    Added Custom StatusBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import { StatusBar } from "expo-status-bar";
 
 export default function SearchScreen() {
+  const [query, setQuery] = useState("");
+
   return (
     <View style={styles.container}>
       <SearchBar />
-      <StatusBar backgroundColor="#448AFFCF" ></StatusBar>
+      <StatusBar backgroundColor="#448AFFCF"></StatusBar>
+      <Text>Searching for : {query}</Text>
     </View>
   );
 }
