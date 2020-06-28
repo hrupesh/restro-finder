802d7f4037d7a0639e8ba2b24e7be751e0cd3e32
hrupesh <rcashok0@gmail.com>
2020-06-28 17:05:37 +0530

    Starting........

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Restro Finder</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark"
+       backgroundColor="#888"
+       hideTransitionAnimation="slide"
+       animated={true}
+       hidden={true} 
+      />
     </View>
   );
 }
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
