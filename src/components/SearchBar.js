8c9c971e9f3741dffc1d33ea7c6751fca909af10
hrupesh <rcashok0@gmail.com>
2020-06-29 01:23:15 +0530

    Creating SearchBar Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,49 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function SearchBar() {
-    return (
-        <View style={styles.container}>
-            <TextInput  placeholder='Search......' />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder="Search" />
+      <TouchableOpacity>
+        <MaterialIcons
+          style={styles.icon}
+          name="search"
+          size={40}
+          color="white"
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#CFD8DC',
-        padding:25,
-        margin:15,
-        marginTop:25,
-        borderRadius:50,
-        shadowColor:'#512DA8',
-        shadowOffset:{
-            height:500,
-            width:500
-        },
-        shadowOpacity:1,
-        shadowRadius:3.14,
-        elevation:25,
-        color:'white'
-    }
-})
+  container: {
+    backgroundColor: "#222",
+    padding: 25,
+    margin: 15,
+    marginTop: 25,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 10,
+    color: "white",
+    flexDirection: "row",
+  },
+  input: {
+    color: "white",
+    fontSize: 20,
+    letterSpacing: 2,
+    width: "100%",
+  },
+  icon: {
+    // position:'absolute',
+    // left:100
+  },
+});
