dcd55ec907f1794466f0bf018e30f843986b3798
hrupesh <rcashok0@gmail.com>
2020-06-30 01:50:35 +0530

    Implementing Yelp API

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "BearerAPY_wrfQRVvIM6qG8LcY-Nv3ZWD7Rx-aboIfk6qEAx1NDNv6uRGPzE5--skfpcapPtlZTTIKDopBYnznhAsO1wk-wP7Jadpb5xqaTtjyZru7jq_MG8Ud4qQQZMn3XnYx",
+      "Bearer APY_wrfQRVvIM6qG8LcY-Nv3ZWD7Rx-aboIfk6qEAx1NDNv6uRGPzE5--skfpcapPtlZTTIKDopBYnznhAsO1wk-wP7Jadpb5xqaTtjyZru7jq_MG8Ud4qQQZMn3XnYx",
   },
 });
