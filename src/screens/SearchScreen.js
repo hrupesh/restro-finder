9549795b5c67bb94618846ff2063a787e3dce30c
hrupesh <rcashok0@gmail.com>
2020-06-30 02:50:33 +0530

    Using Hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import SearchBar from "../components/SearchBar";
 import { StatusBar } from "expo-status-bar";
