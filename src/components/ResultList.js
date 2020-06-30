7782b89ed54ed50156168787ae4b0d559e36f2d5
hrupesh <rcashok0@gmail.com>
2020-07-01 01:29:42 +0530

    Using Hooks

---
+++ src/components/ResultList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ResultList() {
+    return (
+        <View>
+            <Text>Result List</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
