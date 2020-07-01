92e34ace2a34c3950d6183322766e650c5410c67
hrupesh <rcashok0@gmail.com>
2020-07-01 23:06:31 +0530

    Using Hooks

--- src/components/Restro.js
+++ src/components/Restro.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, ImageBackground } from "react-native";
 export default function Restro({ restro }) {
   const image = { uri: restro.image_url };
   return (
-    <ImageBackground  source={image} style={styles.imgcontainer}>
+    <ImageBackground source={image} style={styles.imgcontainer}>
       <Text style={styles.name}>Name is {restro.name}</Text>
     </ImageBackground>
   );
@@ -12,28 +12,21 @@ export default function Restro({ restro }) {
 
 const styles = StyleSheet.create({
   imgcontainer: {
-    borderTopLeftRadius: 20,
-    borderTopRightRadius:20,
-    borderBottomLeftRadius:20,
-    borderBottomRightRadius:20,
-    borderWidth:5,
-    borderColor:'red',
     // flex: 1,
     // resizeMode: "cover",
     justifyContent: "flex-end",
-    height:160,
-    width:270,
-    padding:10,
-    marginHorizontal:15,
-    marginVertical:10,
-    shadowColor:'#000',
-    shadowOffset:{
-        height:5,
-        width:5
+    height: 160,
+    width: 270,
+    padding: 10,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 5,
+      width: 5,
     },
-    shadowOpacity:0.5,
-    shadowRadius:10,
-    elevation:2,
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 2,
   },
-
 });
