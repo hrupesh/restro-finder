71ee08d97623433b2aff520fdb0d11ab88678a95
hrupesh <rcashok0@gmail.com>
2020-07-04 02:34:28 +0530

    Added async loader

--- src/screens/RestroScreen.js
+++ src/screens/RestroScreen.js
@@ -1,14 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import yelp from "../api/yelp";
 
 export default function RestroScreen({ navigation }) {
   //   console.log(props);
   const id = navigation.state.params.id;
+  const [result, setResult] = useState([]);
+
+  const getResult = async (id) => {
+    const res = await yelp.get(`/${id}`);
+    setResult(res.data);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
 
   return (
     <View>
-      <Text>This is Restro Screen!</Text>
-      <Text>ID : {id}</Text>
+      {result.length === 0 ? (
+        <Image
+          style={styles.loader}
+          source={{
+            uri:
+              "https://i.ya-webdesign.com/images/transparent-welcome-gif-background-3.gif",
+          }}
+        />
+      ) : (
+        <View>
+          <Text>This is Restro Screen!</Text>
+          <Text>
+            ID : {id} , Result : {result.name}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
