df2fcdf7e35808007e30445932221c846a45bd95
hrupesh <rcashok0@gmail.com>
2020-07-04 02:25:59 +0530

    Added async loader

--- src/screens/RestroScreen.js
+++ src/screens/RestroScreen.js
@@ -1,26 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function RestroScreen({ navigation }) {
   //   console.log(props);
   const id = navigation.state.params.id;
-
-  const searchAPI = async (searchTerm) => {
-    try {
-      setResults([]);
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "New York",
-        },
-      });
-      setResults(response.data.businesses);
-      // console.log(results);
-    } catch (e) {
-      setError("Something went wrong 🥴");
-    }
-  };
 
   return (
     <View>
