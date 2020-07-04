ed229e38334f2f7bc16089757dba99f7b8509b61
hrupesh <rcashok0@gmail.com>
2020-07-04 17:33:51 +0530

    Added async loader

--- src/screens/RestroScreen.js
+++ src/screens/RestroScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import yelp from "../api/yelp";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 
 export default function RestroScreen({ navigation }) {
   //   console.log(props);
@@ -12,7 +13,8 @@ export default function RestroScreen({ navigation }) {
     const res = await yelp.get(`/${id}`);
     setResult(res.data);
     console.log(result);
-    setPhotos(result.photos);
+    setPhotos(res.data.photos);
+    console.log(photos);
   };
 
   useEffect(() => {
@@ -20,24 +22,47 @@ export default function RestroScreen({ navigation }) {
   }, []);
 
   return (
-    <View>
-      {result === null ? (
-        <Image
-          style={styles.loader}
-          source={{
-            uri:
-              "https://i.ya-webdesign.com/images/transparent-welcome-gif-background-3.gif",
-          }}
-        />
-      ) : (
-        <View>
-          <Text>This is Restro Screen!</Text>
-          <Text>ID : {id}</Text>
-          <Text>Result : {result.name}</Text>
-        </View>
-      )}
+    <View style={styles.container}>
+      <Text style={styles.text}> {result.name}</Text>
+      <ScrollView style={styles.scrollcontainer}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <Image source={{ uri: photos[0] }} style={styles.img} />
+        <Image source={{ uri: photos[1] }} style={styles.img} />
+        <Image source={{ uri: photos[2] }} style={styles.img} />
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 32,
+    letterSpacing: 1,
+    fontWeight: "400",
+    textShadowColor: "#aaa",
+    textShadowOffset: {
+      width: 4,
+      height: 2,
+    },
+    textShadowRadius: 5,
+    marginVertical: 20,
+  },
+  img: {
+    height: 600,
+    width: "100%",
+    marginVertical: 20,
+    borderRadius:10
+  },
+  scrollcontainer:{
+      height:600,
+      width:'100%',
+  }
+});
